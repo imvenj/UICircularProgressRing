50f7971ace53ee2b4b72d9be41cdd593757a9745
Venj <ersaclarke@gmail.com>
2019-03-26 14:36:56 +0800

    Fix build issue under Xcode 10.2; Swift 5.

--- src/UICircularProgressRing/UICircularRingLayer.swift
+++ src/UICircularProgressRing/UICircularRingLayer.swift
@@ -73,7 +73,7 @@ class UICircularRingLayer: CAShapeLayer {
 
     // Returns whether or not a given property key is animatable
     static func isAnimatableProperty(_ key: String) -> Bool {
-        return animatableProperties.index(of: key) != nil
+        return animatableProperties.firstIndex(of: key) != nil
     }
 
     // MARK: Init
@@ -265,7 +265,8 @@ class UICircularRingLayer: CAShapeLayer {
 
         case .bordered(let borderWidth, let borderColor):
             let center: CGPoint = CGPoint(x: bounds.midX, y: bounds.midY)
-            let offSet = max(ring.outerRingWidth, ring.innerRingWidth) / 2
+            let ringWidth = max(ring.outerRingWidth, ring.innerRingWidth) / 2
+            let offSet = ringWidth
                             + ((valueKnobStyle?.size ?? 0) / 4)
                             + (borderWidth * 2)
             let outerRadius: CGFloat = min(bounds.width, bounds.height) / 2 - offSet
